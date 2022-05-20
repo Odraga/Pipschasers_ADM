7b7fd3fbf48a84ca805ce985f992ab870e869911
GABRIEL EDUARDO VARGAS RODRIGUEZ <gabrielvr.dev@gmail.com>
2022-05-20 01:49:56 -0400

    Agregar archivos de proyecto.

---
+++ Pipschasers_ADM/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Pipschasers_ADM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnCrearPDF = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(38, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(184, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(35, 13);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // btnCrearPDF
+            // 
+            this.btnCrearPDF.Location = new System.Drawing.Point(84, 104);
+            this.btnCrearPDF.Name = "btnCrearPDF";
+            this.btnCrearPDF.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearPDF.TabIndex = 2;
+            this.btnCrearPDF.Text = "Crear PDF";
+            this.btnCrearPDF.UseVisualStyleBackColor = true;
+            this.btnCrearPDF.Click += new System.EventHandler(this.btnCrearPDF_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnBuscar.Location = new System.Drawing.Point(38, 56);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(184, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar Imagen";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 139);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnCrearPDF);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnCrearPDF;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Pipschasers_ADM/Form1.cs
@@ -0,0 +1,91 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pipschasers_ADM
+{
+    public partial class Form1 : Form
+    {
+        private byte[] data;
+        private string imgPath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearPDF_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+
+            string filePath = @"Template\";
+
+            string fileNameExisting = @"impuestos.pdf";
+
+            string fileNameNew = @"prueba_" + nombre.Replace(" ", "").Trim() + ".pdf";
+
+            string fullNewPath = filePath + fileNameNew;
+
+            string fullExistingPath = filePath + fileNameExisting;
+
+            using (var existingFileStream = new FileStream(fullExistingPath, FileMode.Open))
+
+            using (var newFileStream = new FileStream(fullNewPath, FileMode.Create))
+            {
+                var pdfReader = new PdfReader(existingFileStream);
+
+                var stamper = new PdfStamper(pdfReader, newFileStream);
+
+                AcroFields fields = stamper.AcroFields;
+
+                BaseColor color;
+                BaseFont font = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+
+                fields.SetFieldProperty("fullName", "textcolor", color = new BaseColor(23,116,131), null);
+                fields.SetFieldProperty("fullName", "textfont", font, null);
+                fields.SetField("fullName", nombre);
+
+                fields.SetFieldProperty("valorDop", "textcolor", color = new BaseColor(255, 0, 0), null);
+                fields.SetField("valorDop", nombre);
+
+                iTextSharp.text.Image imagen = iTextSharp.text.Image.GetInstance(imgPath);
+
+                PushbuttonField button = fields.GetNewPushbuttonFromField("imgTransferencia");
+                button.Layout = PushbuttonField.LAYOUT_ICON_ONLY;
+                button.ProportionalIcon = true;
+                button.Image = imagen;
+
+                fields.ReplacePushbuttonField("imgTransferencia", button.Field);
+
+                stamper.FormFlattening = true;
+
+                stamper.Close();
+                pdfReader.Close();
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Buscar Imagen";
+            openFileDialog1.Filter = "Image files (*.jpeg, *.png) | *.jpeg; *.png";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                data = File.ReadAllBytes(openFileDialog1.FileName);
+                imgPath = openFileDialog1.FileName;
+            }
+        }
+    }
+}
